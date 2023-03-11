4ca92671c84c591664c1ad6a80f979dc5652f8ef
JesS <90232280+waili990@users.noreply.github.com>
2023-03-11 17:01:40 +0800

    update with new image
    and landing page

--- landingpage.php
+++ landingpage.php
@@ -9,41 +9,29 @@ if (!isset($_SESSION["username"])) {
 }
 ?>
 <html>
-<head>
-<title>Hej</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
-.w3-row-padding img {margin-bottom: 12px;}
-/* Remove margins from "page content" on small screens */
-@media only screen and (max-width: 600px) {#main {margin-left: 0}}
-</style>
-</head>
-<body class="w3-black">
-
-
-<!-- Page Content -->
-<div class="w3-padding-large" id="main">
-  <!-- Header/Home -->
-  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
-    <h1 class="w3-jumbo"><span class="w3-hide-small">I'm</span> Ice Bear.</h1>
-    <p>The Strongest, The Cleverest</p>
-    <img src="images/Ice_bear_slay.jpg" alt="boy" class="w3-image" width="992" height="1108">
-  </header>
-
-  <!-- About Section -->
-  <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
-    <h2 class="w3-text-light-grey">Ice Bear</h2>
-    <hr style="width:200px" class="w3-opacity">
-    <p>Ice Bear is the youngest of the trio, but is, undoubtedly, the strongest, the cleverest, and, in some respects, the most mature of them. He was able to rescue his older brothers from certain death without too much hassle and is quick to jump into action if he finds a threat arising. He cares for both of his brothers dearly. He tends to do most of the chores of the house, though he doesn't seem to mind this. Despite his willingness to pitch in where the others don't, he still takes days off to relax and unwind.
-
-Not only is he a smart and strong bear, but he's also essential to the Bears' brotherhood.
-    </p>
-</div>
-  
-</body>
+    <head>
+        <?php
+            include "header.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "nav.php";
+        ?>
+        <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <h1><span>I'm</span> Ice Bear.</h1>
+			<p>The Strongest, The Cleverest</p>
+			<img src="images/Ice_bear_slay.jpg" alt="boy" width="992" height="1108">
+        </div>
+      </div>
+    </div>
+  </div>
+        <?php
+            include "footer.php";
+        ?>
+    </body>
 </html>
